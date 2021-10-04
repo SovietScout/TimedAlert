b40c9a6f604149ad52c224db5c3df05a08e11db9
Ritobhash <ritobhash@gmail.com>
2021-10-04 22:01:00 +0530

    RefactorTimers & GenerateSchedule merged

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import time
 import sched
 import argparse
 import configparser
+from datetime import timedelta
 from datetime import datetime as dt
 from datetime import time as dttime
-from datetime import timedelta
 
 from notifypy import Notify
+
 
 NAME = 'Timed Alert'
 ICON = 'rsc/icon.png'
@@ -18,6 +19,7 @@ class TimedAlert:
         self.logPrint('[Press Ctrl+C to exit]')
 
         self.remindBefore = 5
+        self.mtf = '{timerKey}'
         self.mrf = '{timerKey} starts in {remindBefore} minutes'
         self.mf = '{timerKey} has started'
 
@@ -38,6 +40,7 @@ class TimedAlert:
         # Load settings
         try:
             self.remindBefore = config.getint('Settings', 'remindBefore')
+            self.mtf = config.get('Settings', 'messageTitleFormat')
             self.mrf = config.get('Settings', 'messageReminderFormat')
             self.mf = config.get('Settings', 'messageAlertFormat')
 
@@ -62,11 +65,14 @@ class TimedAlert:
         )
 
     def logPrint(self, value: str) -> None:
-        # 2021-08-10 19:25:00 {value}
-        print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")} {value}')
+        # 2021-08-10 08:30:00 {value}
+        # print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")} {value}')
+
+        # 08:30:00 {value}
+        print(f'{dt.now().strftime("%H:%M:%S")} {value}')
 
     def notify(self, timerName: str, reminder: bool) -> None:
-        self.notification.title = timerName
+        self.notification.title = self.mtf.format(timerKey=timerName)
 
         if reminder:
             self.notification.message = self.mrf.format(
@@ -79,36 +85,33 @@ class TimedAlert:
         self.logPrint((f'{timerName} - {"Reminder" if reminder else "Alert"}'
                        ' notification sent'))
 
-    def refactorTimer(self) -> None:
-        # Replaces time strings to DateTime Objects
+    def generateSchedule(self) -> tuple:
+        # timer [0] = dtObject, [1] = (timerName: str, reminder: bool)
 
         try:
             for name, timer in self.timers.items():
+                # Replaces time strings with Time objects
                 time = dt.strptime(timer, '%H:%M')
-                timeDtObject = dttime(time.hour, time.minute)
-                self.timers[name] = dt.combine(dt.now(), timeDtObject)
-            self.logPrint('Read successful')
+                timeDT = dttime(time.hour, time.minute)
+
+                # Yield Reminders
+                if self.remindBefore > 0:
+                    timer = dt.combine(dt.now(), timeDT)
+                    reminderTime = timer - timedelta(minutes=self.remindBefore)
+                    if reminderTime > dt.now():
+                        reminder = (reminderTime, (name, True))
+                        yield reminder
+
+                # Yield Alert
+                timer = dt.combine(dt.now(), timeDT)
+                if timer > dt.now():
+                    alert = (timer, (name, False))
+                    yield alert
+
         except ValueError:
             self.logPrint('Timer section set incorrectly')
 
-    def generateSchedule(self) -> tuple:
-        # timer[0] = dtObject, [1] = name: str, [2] = reminder: bool
-
-        for name, timer in self.timers.items():
-            # Yield Reminders
-            if self.remindBefore > 0:
-                reminderTime = timer - timedelta(minutes=self.remindBefore)
-                if reminderTime > dt.now():
-                    reminder = (reminderTime, name, True)
-                    yield reminder
-
-            # Yield Alert
-            if timer > dt.now():
-                alert = (timer, name, False)
-                yield alert
-
     def run(self) -> None:
-        self.refactorTimer()
         schedule = self.generateSchedule()
 
         self.logPrint('Schedule:')
@@ -117,11 +120,11 @@ class TimedAlert:
             try:
                 timer = next(schedule)
                 self.scheduler.enterabs(
-                    timer[0].timestamp(), 1, self.notify, (timer[1], timer[2]))
+                    timer[0].timestamp(), 1, self.notify, timer[1])
 
                 print((
-                    ' · '
-                    f'{timer[1]} {"reminder" if timer[2] else "alert"} '
+                    ' | '
+                    f'{timer[1][0]} {"reminder" if timer[1][1] else "alert"} '
                     f'at {timer[0].strftime("%H:%M")}'))
 
             except StopIteration:
@@ -138,5 +141,5 @@ class TimedAlert:
 
 
 if __name__ == "__main__":
-    alert = TimedAlert()
-    alert.run()
+    timedAlert = TimedAlert()
+    timedAlert.run()
